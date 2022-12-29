1a40a38f71fa708140f18038223a5c9f1cd8a125
pravin817 <83346334+pravin817@users.noreply.github.com>
2022-12-29 21:53:46 +0530

    Day 2 : 29-12-2022

--- asteroidCollision.cpp
+++ asteroidCollision.cpp
@@ -80,4 +80,87 @@ class Solution {
    Time Complexity : O(n)
    Space Complexity : O(n)
 */   
-   
+
+/* *************************************************************************************************************** */
+
+// Same Problem on Leetcode
+// Problem Link : https://leetcode.com/problems/asteroid-collision/description/
+
+class Solution {
+  public:
+    vector<int> asteroidCollision(vector<int> &asteroids) {
+       
+       int N = asteroids.size();
+       
+        // create stack
+        
+        stack<int>s;
+        
+        int i = 0;
+        while(i < N)
+        {
+            if(s.empty())
+            {
+                s.push(asteroids[i]);
+                i++;
+            }
+            
+            else
+            {
+                // check for the valid condition
+                // + +
+                // - -
+                // - +
+                if((asteroids[i] > 0 && s.top() > 0) || (asteroids[i] < 0 && s.top() < 0) || (asteroids[i] > 0 && s.top() < 0))
+                {
+                    s.push(asteroids[i]);
+                    i++;
+                }
+                
+                else if(s.top() > 0 && asteroids[i] < 0)
+                {
+                    int x = s.top();
+                    s.pop();
+                    
+                    // Both the elements are same 
+                    
+                    if(x == abs(asteroids[i]))
+                        i++;
+                    else if(x > abs(asteroids[i]))
+                    {
+                        s.push(x);
+                        i++;
+                    }
+                    else if(x < abs(asteroids[i]))
+                    {
+                        continue;
+                    }
+                }
+            }
+        }
+        
+        
+        // load the data from the stack to vector and reverse it and return 
+        
+        vector<int>ans;
+        
+        while(!s.empty())
+        {
+            int temp = s.top();
+            s.pop();
+            ans.push_back(temp);
+        }
+        
+        // Now reverse the vector 
+        
+        reverse(ans.begin(), ans.end());
+        
+        return ans;
+    }
+};
+
+/* 
+   Analysis:
+   Time Complexity : O(n)
+   Space Complexity : O(n)
+*/   
