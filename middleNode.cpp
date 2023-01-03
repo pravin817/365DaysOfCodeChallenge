454225a12d881abf2a0c85a8d3ea18b63924f8d5
pravin817 <83346334+pravin817@users.noreply.github.com>
2023-01-03 11:20:51 +0530

    Day 6 : 02-01-2023

---
+++ middleNode.cpp
@@ -0,0 +1,45 @@
+/*
+  Date : 2/1/2023
+  Platform : Leetcode
+  Problem Link : https://leetcode.com/problems/middle-of-the-linked-list/description/
+*/  
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        int len = 0;
+        struct ListNode *p = head;
+        while(p!=NULL)
+        {
+            len++;
+            p = p->next;
+        }
+
+        int middleLength = (len/2) + 1;
+
+        p = head;
+
+        for(int i = 0;i<middleLength-1;i++)
+        {
+            p = p->next;
+        }
+        
+        return p;
+    }
+};
+
+/*
+  Analysis:
+  Time Complexity : O(n)
+  Space Complexity : O(1)
+*/  
