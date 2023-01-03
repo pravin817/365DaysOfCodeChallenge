4a148d8d7b7306d4859fe03f17e1e907b0d73129
pravin817 <83346334+pravin817@users.noreply.github.com>
2023-01-03 11:30:06 +0530

    Day 6 : 02-01-2023

--- middleNode.cpp
+++ middleNode.cpp
@@ -51,3 +51,57 @@ public:
   Time Complexity : O(n)
   Space Complexity : O(1)
 */  
+
+
+-----------------------------------------------------------------------------------------------------------
+  
+/* 
+   Approach : 2 
+   Slow and Fast Pointer
+*/   
+   
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+  
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        
+        // Now using the slow and fast pointer
+        // slow pointer will move one node at a time while fast node will move the two nodes at a time.
+
+        // let initilize the node to head
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        // check if the fast reach at the last and fast->next is not NULL
+
+        while(fast != NULL && fast->next !=NULL)
+        {
+            // Move the pointer
+
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Hence the slow pointer is at the middle of the linkedlist and return it.
+        
+        return slow;
+    }
+};
+
+
+/* 
+    Analysis:
+    Time Complexity : O(n)
+    Space Complexity : O(1)
+*/      
