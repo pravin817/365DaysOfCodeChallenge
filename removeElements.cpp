b0865729a902af2eb21327b09f0a4a87cd133f41
pravin817 <83346334+pravin817@users.noreply.github.com>
2023-01-04 22:46:39 +0530

    Day 8 : 04-01-2023

--- removeElements.cpp
+++ removeElements.cpp
@@ -66,3 +66,36 @@ public:
   Time Complexity : O(n)
   Space Complexity : O(1)
 */  
+
+
+
+// recursive Solution
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if(head == NULL)
+            return head;
+
+        head->next = removeElements(head->next, val);
+
+        return head->val==val?head->next :head;    
+    }
+};
+
+/* 
+    Analysis
+    Time Complexity : O(n)
+    Space Complexity : O(n)
+*/    
