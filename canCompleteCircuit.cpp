ea988ea59f86534d38b118af1c77e07fc9cd8a1e
pravin817 <83346334+pravin817@users.noreply.github.com>
2023-01-07 23:44:24 +0530

    Day 11 : 07/01/2023

--- canCompleteCircuit.cpp
+++ canCompleteCircuit.cpp
@@ -50,3 +50,84 @@ public:
     Time Complexity : O(n)
     Space Complexity : O(1)
 */    
+
+
+
+
+/* ************************************************  II ******************************************************** */
+// Problem Link : https://leetcode.com/problems/add-two-numbers/description/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        // Create the three linkedlist
+
+        ListNode *ans = new ListNode(0);
+        ListNode *p = l1,*q = l2, *curr = ans;
+
+        int carry = 0;
+        while(p!=NULL || q!=NULL)
+        {
+            // Take the value from linked list l1
+            int x = (p!=NULL) ? p->val : 0;
+
+            // Take the value from linked list l2
+            int y = (q!=NULL) ? q->val : 0;
+
+            // Calculate the sum
+
+            int sum = carry+x+y;
+
+            // Update the current linked lisy
+            curr->next = new ListNode(sum%10);
+            
+            // Update the carry
+            carry = sum/10;
+
+            // Move the pointer to the next node
+            curr = curr->next;
+
+
+            // Move to the next node of the linked lists
+
+            if(p!=NULL)
+                p = p->next;
+
+            if(q!=NULL)
+                q = q->next;    
+        }
+
+
+        // If there is the carry then add it to the curr
+
+        if(carry>0)
+            curr->next = new ListNode(carry);
+
+        // return the ans->next because ans start with the 0.
+        return ans->next;    
+    }
+
+};
+
+
+/*
+    Analysis:
+    Time Complexity: O(n)
+    Space Complexity : O(max(l1,l2));
+*/    
+
+
+
+
+
+
