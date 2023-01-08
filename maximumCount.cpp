19494ee484d1950e0683b79372641d979a8ae3a1
pravin817 <83346334+pravin817@users.noreply.github.com>
2023-01-08 23:26:58 +0530

    Day 12 : 08/01/2023

--- maximumCount.cpp
+++ maximumCount.cpp
@@ -26,3 +26,34 @@ public:
   Time Complexity : O(n)
   Space Complexity : O(1)
 */  
+
+
+
+/* ******************************** II ************************************** */
+
+/*
+  Day 12: 08/01/2023
+  Platform : Leetcode
+  Problem Link : https://leetcode.com/problems/number-of-1-bits/description/
+*/
+
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+
+        while(n>0)
+        {
+            count+=(n&1);
+            n = n>>1;
+        }
+        return count;
+    }
+};
+
+/*
+    Analysis:
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+*/    
