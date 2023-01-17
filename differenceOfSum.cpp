087eab858d0be0f65dc592cb723efd322008c29f
pravin817 <83346334+pravin817@users.noreply.github.com>
2023-01-17 23:44:05 +0530

    Day 21: 17/01/2023

--- differenceOfSum.cpp
+++ differenceOfSum.cpp
@@ -36,3 +36,20 @@ public:
     Time Complexity : O(n) --> Where n is the number of the elements in the input vector, As we iterate through each element in the vector onces.
     Space Complexity : O(1) ---> as we are using a conastant amount of extra space to store the variable sum and count.
 */    
+
+// Second Question : https://practice.geeksforgeeks.org/problems/middle-of-three2926/1?page=2&status[]=unsolved&category[]=Mathematical&sortBy=submissions
+
+class Solution{
+  public:
+    int middle(int A, int B, int C){
+        
+        return ((A>=B && A <= C) || (A>=C && A<=B)) ?A:((B>=A && B<=C) || (B >= C && B <= A)) ?B:C;   
+       
+    }
+};
+/*
+    Analaysis:
+    TIme Complexity: O(1)
+    Space Complexity: O(1)
+*/ 
+
