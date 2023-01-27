3b9ab33abcbd709db102997df01d598d2a7c8fae
Pravin Mhaske <pravinmhaske817@gmail.com>
2023-01-27 23:07:09 +0530

    Day 31: 27/01/2023

--- search.cpp
+++ search.cpp
@@ -23,3 +23,36 @@ public:
     Time Complexity:O(n)
     Space Complexity: O(1)
 */    
+
+
+// Binary Search
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        // As the array is sorted we can apply the binary search
+
+        int low = 0;
+        int high = nums.size()-1;
+
+        while(low <= high)
+        {
+            int mid = low+(high - low)/2;
+
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] > target)
+                high = mid - 1;
+            else
+                low =  mid + 1;        
+        }
+        return -1;
+    }
+};
+
+
+/*
+    Analysis:
+    Time complexity: O(logn)
+    Space complexity: O(1)
+*/    
