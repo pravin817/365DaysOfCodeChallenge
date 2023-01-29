b4c31a50785f52e45d0978754f561ba425400604
Pravin Mhaske <pravinmhaske817@gmail.com>
2023-01-29 21:56:59 +0530

    Day 33: 29/01/2023

---
+++ reverse.cpp
@@ -0,0 +1,28 @@
+/*
+  Day 33: 29/01/2023
+  Platform : Leetcode
+  Problem Link : https://leetcode.com/problems/reverse-integer/description/
+*/
+
+class Solution {
+public:
+    int reverse(int x) {
+        long long rev = 0;
+
+        while(x!=0)
+        {
+            int rem = x % 10 ;
+            rev = rev *10+ rem;
+            x = x / 10;
+        }    
+
+        return (rev > INT_MAX || rev < INT_MIN) ? 0:rev; 
+    }
+};
+
+
+/* 
+    Analysis:
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+*/
