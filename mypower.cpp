0a896c4fc4a6d168f2c4c05704a0e607a582a260
Pravin Mhaske <pravinmhaske817@gmail.com>
2023-01-31 23:49:45 +0530

    Day 35: 31/01/2023

---
+++ mypower.cpp
@@ -0,0 +1,13 @@
+/*
+  Day 35: 31/01/2023
+  Platform : Leetcode
+  Problem Link : https://leetcode.com/problems/powx-n/description/
+*/
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = pow(x,n);
+        return ans;
+    }
+};
