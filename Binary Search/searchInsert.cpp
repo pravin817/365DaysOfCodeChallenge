16680057b7b4eed1a88d6451328a28a8fe097e89
Pravin Mhaske <pravinmhaske817@gmail.com>
2023-02-20 22:40:26 +0530

    Day 51: 20/02/2023

---
+++ Binary Search/searchInsert.cpp
@@ -0,0 +1,42 @@
+/*
+  Day 51: 20/02/2023
+  Platform : Leetcode
+  Problem Link : https://leetcode.com/problems/search-insert-position/description/
+*/
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        // As the array is sorted we can apply the binary search
+        // Let's apply the binary search
+
+        int low = 0;
+        int high = nums.size()-1;
+
+        while(low <= high)
+        {
+            // find the mid
+
+            int mid = low + (high - low)/2;
+
+            // if the target element is present at the mid position
+
+            if(nums[mid] == target)
+                return mid;
+
+            if(nums[mid]<target)
+                low = mid + 1;
+            else
+                high = mid - 1;        
+        }
+
+        // If the element is not present then the position of the element will be at low hence return low
+        return low;
+    }
+};
+
+/*
+    Analysis:
+    Time Complexity: O(logn)
+    Space Complexity : O(1)
+*/    
