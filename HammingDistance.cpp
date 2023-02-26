efc8d64ed545e7fdd3cc2973ec1237306d03f3ff
Pravin Mhaske <pravinmhaske817@gmail.com>
2023-02-26 23:21:59 +0530

    Day 56: 26/02/2023

---
+++ HammingDistance.cpp
@@ -0,0 +1,30 @@
+/*
+    Day 56: 26/02/2023
+    Platform : Leetcode
+    Link : https://leetcode.com/problems/hamming-distance/description/
+*/
+
+// Approach :
+// 1) Take the xor 
+// 2) Count the set bit in the number and return 
+
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int num = x ^ y;
+        int count = 0;
+
+        while(num)
+        {
+            count+=num%2;
+            num >>= 1;
+        }
+        return count;
+    }
+};
+
+/*
+    Analysis:
+    Time Complexity : O(n)
+    Space Complexity : O(1)
+*/    
