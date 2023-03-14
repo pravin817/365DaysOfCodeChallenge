a0362d22e35419435729a5d3a7ed4f7678d2aded
Pravin Mhaske <pravinmhaske817@gmail.com>
2023-03-14 23:31:50 +0530

    Day 67: 14/03/2023

---
+++ Day 67/sumNumbers.cpp
@@ -0,0 +1,26 @@
+/*
+  Day 67: 14/03/2023
+  Platform : Leetcode
+  Problem Link : https://leetcode.com/problems/sum-root-to-leaf-numbers/description/
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+ 
+class Solution {
+public:
+    int sumNumbers(TreeNode* root, int num=0) {
+        return root->left == root->right ? num * 10 + root->val :
+            ((root->left ? sumNumbers(root->left, num * 10 + root->val) : 0) + 
+            (root->right ? sumNumbers(root->right, num * 10 + root->val) : 0));
+    }
+};
